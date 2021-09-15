195bd379db4de04bbf315919d0f2c11f0e445dfb
ofazli <fazliorcun@gmail.com>
2021-09-14 21:31:18 -0400

    Api

--- src/test/java/get_http_request_method/Get06.java
+++ src/test/java/get_http_request_method/Get06.java
@@ -2,11 +2,13 @@ package get_http_request_method;
 
 import base_urls.HerOkuAppBaseUrl;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import org.junit.Test;
 import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertEquals;
 
 public class Get06 extends HerOkuAppBaseUrl {
 
@@ -43,15 +45,29 @@ public class Get06 extends HerOkuAppBaseUrl {
 
         //4. Step : Do Assertion
         //1.Way :
+//        response.
+//                then().assertThat().statusCode(200).
+//                contentType(ContentType.JSON).
+//                body("firstname",equalTo("Mary"),
+//                        "lastname", equalTo("Jackson"),
+//                        "totalprice", equalTo(333),
+//                        "depositpaid", equalTo(false),
+//                        "bookingdates.checkin", equalTo("2020-01-03"),
+//                        "bookingdates.checkout", equalTo("2021-05-16"),
+//                        "additionalneeds",equalTo("Breakfast"));
+
+        //2.Way : Use JsonPath. JsonPath is a class and it has many useful methods to navigate inside the Json Data
         response.
                 then().assertThat().statusCode(200).
-                contentType(ContentType.JSON).
-                body("firstname",equalTo("Mary"),
-                        "lastname", equalTo("Jackson"),
-                        "totalprice", equalTo(333),
-                        "depositpaid", equalTo(false),
-                        "bookingdates.checkin", equalTo("2020-01-03"),
-                        "bookingdates.checkout", equalTo("2021-05-16"),
-                        "additionalneeds",equalTo("Breakfast"));
+                contentType(ContentType.JSON);
+
+        //Create JsonPath object from response object
+        JsonPath json = response.jsonPath();
+        assertEquals ("First name is not matching","Sally", json.getString("firstname"));
+        assertEquals ("Last name is not matching","Smith", json.getString("lastname"));
+        assertEquals ("Total Price is not matching",224, json.getInt("totalprice"));
+        assertEquals ("Deposit Paid value is not matching",true, json.getBoolean("depositpaid"));
+        assertEquals ("Checkin date is not matching","2021-04-12", json.getString("bookingdates.checkin"));
+        assertEquals ("Checkout date is not matching","2021-05-07", json.getString("bookingdates.checkout"));
     }
 }
