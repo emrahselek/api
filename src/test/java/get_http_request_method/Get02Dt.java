2598d4d657bcf813ba782e28be16a76ae5016e6e
ofazli <fazliorcun@gmail.com>
2021-09-15 15:09:48 -0400

    Api

--- src/test/java/get_http_request_method/Get02Dt.java
+++ src/test/java/get_http_request_method/Get02Dt.java
@@ -1,4 +1,40 @@
 package get_http_request_method;
 
-public class Get02Dt {
+import base_urls.HerOkuAppBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class Get02Dt extends HerOkuAppBaseUrl {
+    /*
+        Given
+            https://restful-booker.herokuapp.com/booking/1001
+        When
+            Send a Get Request to the URL
+        Then
+            HTTP Status code should be 404
+        And
+            Status Line should be HTTP/1.1 404 Not Found
+        And
+            Response body contains “Not Found”
+        And
+            Response body does not contain “TechProEd”
+        And
+            Server is “Cowboy”
+     */
+    @Test
+    public void get02(){
+        //1. Step : Set the URL
+        spec.pathParams("first","booking", "second",1001);
+
+        //2. Step : Set the expected date
+
+        //3. Step : Send the request and get the response
+        Response response = given().spec(spec).when().get("/{first}/{second}");
+        response.prettyPrint();
+
+        //4. Step : Do assertions
+        response.then().assertThat().statusCode(404).statusLine("HTTP/1.1 404 Not Found");
+    }
 }
