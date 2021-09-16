66555a7fad2fbc980eb45a59b3e5bb704dcc965c
ofazli <fazliorcun@gmail.com>
2021-09-15 20:09:11 -0400

    Api

---
+++ src/test/java/base_urls/RestApiExampleUrl.java
@@ -0,0 +1,14 @@
+package base_urls;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
+
+public class RestApiExampleUrl {
+    protected RequestSpecification spec;
+    @Before
+    public void setUp(){
+        spec = new RequestSpecBuilder().setBaseUri("http://dummy.restapiexample.com").build();
+    }
+}
+
