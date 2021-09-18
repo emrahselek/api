ca90a432e2a0106e9c4495fbd4864448e7518713
ofazli <fazliorcun@gmail.com>
2021-09-18 16:14:38 -0400

    Api

--- src/test/java/post_http_request_method/post03.java
+++ src/test/java/post_http_request_method/post03.java
@@ -1,7 +1,15 @@
 package post_http_request_method;
 
 import base_urls.AgromonitoringApiBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.Test;
+import test_data.AgroMonitoringTestData;
+import java.util.*;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
 
 public class post03 extends AgromonitoringApiBaseUrl {
     /*
@@ -77,10 +85,37 @@ public class post03 extends AgromonitoringApiBaseUrl {
 
     @Test
     public void post03(){
-        //1.Step Set the url
-    spec.pathParams("first", "agro", "second", 1.0, "third", "polygons").
-            queryParam("appid","f4ffe3b2ef1fcb3600ab1d7fbc88c2f0");
+        //1.Step: Set the url
+        spec.pathParams("first", "agro", "second", 1.0, "third", "polygons").
+                queryParam("appid", "f4ffe3b2ef1fcb3600ab1d7fbc88c2f0");
+        //2.Step: Set the expected data
+        AgroMonitoringTestData requestBody = new AgroMonitoringTestData();
+        Map<String, Object> requestBodyMap = requestBody.requestBodySetUp();
+        //3.Step: Send the request and get the response
+        Response response = given().
+                spec(spec).
+                contentType(ContentType.JSON).
+                body(requestBodyMap).
+                when().
+                post("/{first}/{second}/{third}");
+        response.prettyPrint();
 
-        //2.Step : Set the expected data
+        //Add more ky-values into the request body
+        requestBodyMap.put("area", 190.9484);
+
+
+        //Use GSON to convert response to a Map
+        Map<String, Object> responseBody = response.as(HashMap.class);
+        System.out.println(responseBody);
+
+        assertEquals(requestBodyMap.get("area"), responseBody.get("area"));
+        assertEquals(requestBodyMap.get("name"), responseBody.get("name"));
+        assertEquals(requestBody.geometrySetUp().get("type"),
+                ((Map)((Map)responseBody.get("geo_json")).get("geometry")).get("type"));
+
+        System.out.println(requestBody.coordinates[0] [1] [0]);
+
+        assertEquals(String.valueOf(requestBody.coordinates[0] [1] [0]),
+                ((Map)((Map)responseBody.get("geo_json")).get("geometry")).get("coordinates").toString().substring(25,34));
     }
 }
